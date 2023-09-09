1e9190abb57a8899c6423bdf63b6d843020ef41b
sameh.elolimy <sameh.elolimy@gmail.com>
2023-09-08 20:04:20 -0700

    1-last_digit.c

---
+++ alx-low_level_programming/0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - this is the starting point
+ * description: last digit of the number stored
+ * Return: 0 (Success)
+*/
+int main(void)
+{
+	int n, digit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	digit = n % 10;
+	if (digit > 5)
+		printf("Last digit of %i is %i and is greater than 5\n", n, digit);
+	else if (digit == 0)
+       printf("Last digit of %i is %i and is 0\n", n, digit);
+	else if (digit < 6 && digit != 0)
+		printf("Last digit of %i is %i and less than 6 and not 0\n", n, digit);
+	return (0);
+}
